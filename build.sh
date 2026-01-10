4562d9d11c6e3782a18ed52c8e240705fa629136
PilotChalkanov <nik.chalkanov@gmail.com>
2026-01-10 22:13:08 +0200

    after build place the image in shared folder

--- build.sh
+++ build.sh
@@ -6,11 +6,13 @@
 git submodule init
 git submodule sync
 git submodule update
+git -C poky checkout -B kirkstone origin/kirkstone
+git -C meta-raspberrypi checkout -B kirkstone origin/kirkstone
 
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
 
-MACHINE="${1:-qemuarm64}"
+MACHINE="${1:-raspberrypi4-64}"
 CONFLINE="MACHINE = \"${MACHINE}\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
@@ -25,15 +27,16 @@ else
 fi
 
 
-bitbake-layers show-layers | grep "meta-aesd" > /dev/null
+bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
 
 if [ $layer_info -ne 0 ];then
-	echo "Adding meta-aesd layer"
-	bitbake-layers add-layer ../meta-aesd
+	echo "Adding meta-raspberrypi layer"
+	bitbake-layers add-layer ../meta-raspberrypi
+	# bitbake-layers add-layer ../meta-aesd
 else
-	echo "meta-aesd layer already exists"
+	echo "meta-raspberrypi layer already exists"
 fi
 
-set -e
-bitbake core-image-aesd
+bitbake core-image-full-cmdline
+cp -p ${PWD}/tmp/deploy/images/raspberrypi4-64/core-image-full-cmdline-raspberrypi4-64.wic.bz2 /media/sf_shared/core-image-full-cmdline-raspberrypi4-64.wic.bz2
