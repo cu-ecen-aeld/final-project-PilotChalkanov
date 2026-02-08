6018ee393d5991675bfdc243809c3f420b27562f
PilotChalkanov <nik.chalkanov@gmail.com>
2026-02-08 13:48:42 +0200

    add to device tree config

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ add_conf_line() {
 # Ensure MACHINE is set in local.conf
 add_conf_line '# This sets the default machine to be raspberrypi4-64:'
 add_conf_line "MACHINE ??= \"$MACHINE\""
+add_conf_line 'RPI_KERNEL_DEVICETREE_OVERLAYS += "overlays/lcd1602-overlay.dtbo"'
 
 echo "Adding meta-raspberrypi layer"
 bitbake-layers add-layer ../meta-raspberrypi
